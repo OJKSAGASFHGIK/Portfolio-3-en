550ba4f49cdc8bcf82dcb5575f861b1478ec9121
Greque <ichijou_raku@hotmail.com>
2024-12-25 17:36:44 -0300

    a

---
+++ public2/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar1 from './sections/navbar/navbar.jsx';
+
+function App() {
+  return (
+    <>
+      <Navbar1 />
+    </>
+  )
+}
+
+export default App
---
+++ public2/src/sections/navbar/navbar.jsx
@@ -0,0 +1,20 @@
+function Navbar1(){
+    return(
+        <>
+        <nav className="navbar navbar-dark bg-dark navbar-expand">
+            <div className="container">
+            <a href="#idHome" className="navbar-brand">CONTATO</a>
+            <div>
+            <ul className="navbar-nav">
+                <li className="nav-item"><a href="" className="nav-link">a1</a></li>
+                <li className="nav-item"><a href="" className="nav-link">a2</a></li>
+                <li className="nav-item"><a href="" className="nav-link">a3</a></li>
+            </ul>
+            </div>
+            </div>
+        </nav>
+        </>
+    );
+}
+
+export default Navbar1
