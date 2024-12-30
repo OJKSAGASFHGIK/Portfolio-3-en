8503b5abee1f04f65cac084fb6453761defb1ffa
Greque <ichijou_raku@hotmail.com>
2024-12-30 02:52:43 -0300

    a

--- public2/src/App.jsx
+++ public2/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar1 from './sections/navbar/Navbar1.jsx';
 import SIntroduction from './sections/sIntroduction/SIntroduction.jsx';
 import Objective from './sections/objective/Objective.jsx';
+import Skills from './sections/skills/Skills.jsx';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Navbar1 theme='sTheme' />
       <SIntroduction theme='bTheme'  />
       <Objective theme='sTheme' myId='idObjective' />
-
+      <Skills theme="bTheme" />
     </>
   )
 }
--- public2/src/sections/skills/Skills.jsx
+++ public2/src/sections/skills/Skills.jsx
@@ -1,11 +1,92 @@
 import './../../assets/css/css.css';
 
-function name({theme="sTheme", myId=""}){
+function Skills({theme="sTheme", myId=""}){
     return(
         <>
-            
+            <section id={myId} className={`container-fluid ${theme}`}>
+                <h3>Proficiências:</h3>
+                <div>
+                    <div>
+                        <table className='table'>
+                            <caption>✔</caption>
+                            <thead>
+                                <tr>
+                                    <th>Competências</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>HTML, CSS, JS</td>
+                                </tr>
+                                <tr>
+                                    <td>React.JS</td>
+                                </tr>
+                                <tr>
+                                    <td>Python, POO</td>
+                                </tr>
+                                <tr>
+                                    <td>Microsoft Office</td>
+                                </tr>
+                            </tbody>
+
+                            {/*<tfoot>
+                                <tr>
+                                    <td></td>
+                                </tr>
+                            </tfoot>*/}
+                        </table>
+                    </div>
+                    <div>
+                        <table>
+                            <caption>✢</caption>
+                            <thead>
+                                <tr>
+                                    <th>Extra:</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Conceitos de POO e lógicas fundamentais</td>
+                                </tr>
+                            </tbody>
+                            {/*<caption>⟳</caption>
+                            <thead>
+                                <tr>
+                                    <th>Atualmente estudando</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>JavaScript</td>
+                                </tr>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td></td>
+                                </tr>
+                            </tfoot>*/}
+                        </table>
+                        <table>
+                            <caption>●</caption>
+                            <thead>
+                                <tr>
+                                    <th>Idiomas:</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Português nativo</td>
+                                </tr>
+                                <tr>
+                                    <td>Inglês intermediário-avançado</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </section>
         </>
     );
 }
 
-export default name
+export default Skills
