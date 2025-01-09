c0b72000aa230da38f59fbdfa6a2b4d215d3531f
Greque <ichijou_raku@hotmail.com>
2025-01-09 16:39:56 -0300

    a

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,6 +5,9 @@ import Skills from './sections/skills/Skills.jsx';
 import SProjects from './sections/sProjects/SProjects.jsx';
 import SFooter from './sections/footer/SFooter.jsx';
 
+import './assets/bootstrap-5.3.3/dist/css/bootstrap.min.css';
+import './assets/bootstrap-5.3.3/dist/js/bootstrap.min.js';
+
 function App() {
   return (
     <>
